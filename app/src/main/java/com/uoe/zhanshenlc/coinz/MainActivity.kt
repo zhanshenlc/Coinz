b77ca5fd0be2da0be2c2259a48b9025a2a2dec7a
Adam LI <zhanshenlc@live.com>
2018-10-24 03:03:31 +0100

    Add a welcome (splash) window

--- app/src/main/java/com/uoe/zhanshenlc/coinz/MainActivity.kt
+++ app/src/main/java/com/uoe/zhanshenlc/coinz/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.uoe.zhanshenlc.coinz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    val SPLASH_TIME_OUT = 4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(object : Runnable{
+            override fun run() {
+                val home = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(home)
+                finish()
+            }
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
