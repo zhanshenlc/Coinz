a818f9aa5caad7378d28c2d6d7b17899c647827b
Adam LI <zhanshenlc@live.com>
2018-12-06 16:47:46 +0000

    Shop activity layout

--- app/src/main/java/com/uoe/zhanshenlc/coinz/ShopActivity.kt
+++ app/src/main/java/com/uoe/zhanshenlc/coinz/ShopActivity.kt
@@ -2,11 +2,21 @@ package com.uoe.zhanshenlc.coinz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class ShopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_shop)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
+
 }
